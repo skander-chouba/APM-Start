a4b7a0750ccf1367c19bf962563ff5df314fdfb6
Skander Chouba <skander.chouba@gmail.com>
2020-07-09 00:40:08 +0100

    Initial Commit

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './Product';
+import { ProductService } from './product.service';
+
+@Component({
+    selector: 'pm-products',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit{
+    errorMessage: string;
+    pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage = false;
+    _listFilter : string;
+    get listFilter(): string{
+        return this._listFilter;
+    }
+    set listFilter(value: string){
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter): this.products;
+    }
+    
+    products: IProduct[] = [];
+    filteredProducts: IProduct[];
+
+    constructor(private productService : ProductService){
+    }
+    
+    
+    ngOnInit(): void {
+        this.productService.getProducts().subscribe({
+            next: products => 
+            {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error: err => this.errorMessage = err
+        });
+        
+    } 
+    toggleImage(): void{
+        this.showImage = ! this.showImage;
+    }
+    performFilter(filterBy: string): IProduct[]{
+        filterBy = filterBy.toLowerCase();
+        return this.products.filter((p: IProduct) => p.productName.toLowerCase().indexOf(filterBy) !== -1)
+    }
+    onRatingClicked(message: string):void{
+        this.pageTitle = 'Product List: ' + message;
+    }
+}
